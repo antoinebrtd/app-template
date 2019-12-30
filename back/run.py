0de34b35cf736eba1a10368d8d729a8a6ead812b
antoinebrtd <abertrand830@gmail.com>
2019-12-30 09:03:10 +0100

    Run app under https

--- back/run.py
+++ back/run.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     logger.info('Starting Template API ...')
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True, ssl_context=('certs/localhost.pem', 'certs/localhost-key.pem'))
     logger.info('End of Template')
