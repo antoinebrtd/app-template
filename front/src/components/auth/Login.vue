cd9ba1531a6bed3c1e26e8da2478a5446e8d9d1c
antoinebrtd <abertrand830@gmail.com>
2020-01-01 15:02:46 +0100

    Refactor email login component

--- front/src/components/auth/Login.vue
+++ front/src/components/auth/Login.vue
@@ -1,29 +1,16 @@
 <template>
   <div class="login">
-    <v-container class="text-xs-center" pt-1 v-if="loaded">
+    <v-container class="text-xs-center" v-if="loaded">
       <v-layout row justify-center>
         <v-flex xs12 sm6>
           <v-card flat id="card" color="transparent">
             <v-card-text primary-title>
-              <v-layout column align-center v-if="!signUpView">
-                <email-login :token="$route.params.token"></email-login>
-                <v-btn text class="button mt-2" color="primary" @click="signUpView = !signUpView">
-                  Don't have an account ? Sign up!
-                </v-btn>
-              </v-layout>
-              <v-layout column align-center v-else>
-                <email-sign-up :token="token"></email-sign-up>
-                <v-btn text class="button mt-2" color="primary" @click="signUpView = !signUpView">
-                  Already have an account ? Log in.
-                </v-btn>
-              </v-layout>
+              <email-login :token="$route.params.token"></email-login>
               <v-card-text class="mt-5">
                 <span>Or</span>
               </v-card-text>
-              <v-card-text class="mt-5">
-                <google-login></google-login>
-                <facebook-login class="mt-5"></facebook-login>
-              </v-card-text>
+              <google-login class="mt-5"></google-login>
+              <facebook-login class="mt-5"></facebook-login>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -36,18 +23,15 @@
   import auth from "@/modules/auth";
   import GoogleLogin from "./util/GoogleLogin";
   import EmailLogin from "./util/EmailLogin";
-  import EmailSignUp from "./util/EmailSignUp";
   import FacebookLogin from "./util/FacebookLogin";
 
   export default {
     name: 'Login',
-    components: {FacebookLogin, EmailSignUp, EmailLogin, GoogleLogin},
+    components: {FacebookLogin, EmailLogin, GoogleLogin},
     data() {
       return {
         loaded: false,
-        signUpView: false,
-        token: this.$route.params.token,
-        error: false
+        token: this.$route.params.token
       }
     },
     created() {
@@ -76,10 +60,5 @@
 
   #card {
     margin: 4em;
-  }
-
-  .button {
-    text-transform: None !important;
-    text-decoration: underline;
   }
 </style>
--- front/src/components/auth/util/EmailLogin.vue
+++ front/src/components/auth/util/EmailLogin.vue
@@ -1,44 +1,83 @@
 <template>
-    <v-card class="elevation-0" style="background-color: transparent">
-        <v-layout row wrap justify-center>
-            <v-flex xs12>
-                <v-form ref="form" lazy-validation>
-                    <v-layout row wrap>
-                        <v-flex xs12>
-                            <v-card-text class="py-0 mt-3">
-                                <v-text-field
-                                        :rules="[rules.emptyEmail, rules.emailNotValid, rules.emailIssue]"
-                                        outline
-                                        label="Email"
-                                        append-icon="mail"
-                                        v-model="email"
-                                        required
-                                ></v-text-field>
-                            </v-card-text>
-                        </v-flex>
-                        <v-flex xs12>
-                            <v-card-text class="py-0 mt-2">
-                                <v-text-field
-                                        v-model="password"
-                                        :rules="[rules.emptyPassword, rules.incorrectPassword]"
-                                        :append-icon="showPassword ? 'visibility_off' : 'visibility'"
-                                        :type="showPassword ? 'text' : 'password'"
-                                        name="input-10-1"
-                                        label="Password"
-                                        outline
-                                        @click:append="showPassword = !showPassword"
-                                        required
-                                ></v-text-field>
-                            </v-card-text>
-                        </v-flex>
-                    </v-layout>
-                </v-form>
-            </v-flex>
-            <v-flex xs11>
-                <v-btn color="secondary" block class="login mt-5" @click="validate()">Log in</v-btn>
-            </v-flex>
-        </v-layout>
-    </v-card>
+    <v-layout column align-center>
+        <v-card class="elevation-0" style="background-color: transparent">
+            <v-layout row wrap justify-center>
+                <v-flex xs12>
+                    <v-form ref="form" lazy-validation>
+                        <v-layout row wrap>
+                            <v-flex xs12>
+                                <v-card-text class="py-0 mt-3">
+                                    <v-text-field
+                                            :rules="[rules.emptyEmail, rules.emailNotValid, rules.emailIssue]"
+                                            outline
+                                            label="Email"
+                                            append-icon="mail"
+                                            v-model="email"
+                                            required
+                                    ></v-text-field>
+                                </v-card-text>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-card-text class="py-0 mt-2">
+                                    <v-text-field
+                                            v-if="!signUp"
+                                            v-model="password"
+                                            :rules="[rules.emptyPassword, rules.incorrectPassword]"
+                                            :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                                            :type="showPassword ? 'text' : 'password'"
+                                            name="input-10-1"
+                                            label="Password"
+                                            outline
+                                            @click:append="showPassword = !showPassword"
+                                            required
+                                    ></v-text-field>
+                                    <v-text-field
+                                            v-else
+                                            v-model="password"
+                                            :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                                            :rules="[rules.emptyPassword, rules.nonValidPassword, rules.passwordLength]"
+                                            :type="showPassword ? 'text' : 'password'"
+                                            name="input-10-1"
+                                            label="Password"
+                                            hint="At least 8 characters"
+                                            counter outline
+                                            @click:append="showPassword = !showPassword"
+                                            required
+                                    ></v-text-field>
+                                </v-card-text>
+                            </v-flex>
+                            <v-flex xs12 v-if="signUp">
+                                <v-card-text class="py-0">
+                                    <v-text-field
+                                            :disabled="this.password.length < 8"
+                                            v-model="confirmPassword"
+                                            :append-icon="showConfirmPassword ? 'visibility_off' : 'visibility'"
+                                            :rules="[rules.match, rules.nonValidPassword]"
+                                            :type="showConfirmPassword ? 'text' : 'password'"
+                                            name="input-10-1"
+                                            label="Confirm password"
+                                            outline
+                                            @click:append="showConfirmPassword = !showConfirmPassword"
+                                            required
+                                    ></v-text-field>
+                                </v-card-text>
+                            </v-flex>
+                        </v-layout>
+                    </v-form>
+                </v-flex>
+                <v-flex xs11>
+                    <v-btn v-if="!signUp" color="secondary" block class="login mt-5" @click="validate()">Log in</v-btn>
+                    <v-btn v-else color="secondary" block class="login mt-5" @click="validate()">Sign up</v-btn>
+                </v-flex>
+            </v-layout>
+        </v-card>
+        <v-btn v-if="!signUp" text class="button mt-2" color="primary" @click="changeView">
+            Don't have an account ? Sign up!
+        </v-btn>
+        <v-btn v-else text class="button mt-2" color="primary" @click="changeView">
+            Already have an account ? Log in.
+        </v-btn>
+    </v-layout>
 </template>
 
 <script>
@@ -53,16 +92,25 @@
         },
         data() {
             return {
-                errorType: undefined,
-                errorMessage: undefined,
+                signUp: false,
+                errorType: null,
+                errorMessage: null,
                 showPassword: false,
-                email: undefined,
+                showConfirmPassword: false,
+                email: null,
                 password: '',
+                confirmPassword: '',
                 rules: {
                     emptyEmail: v => !!v || 'Please enter your email',
-                    emailNotValid: v => /.+@.+/.test(v) || 'This email is not valid.',
-                    emptyPassword: v => !!v || 'Please enter your password.',
+                    emailNotValid: v => /.+@.+/.test(v) || 'This email is not valid',
+                    emptyPassword: v => !!v || 'Please enter your password',
+                    passwordLength: v => v.length >= 8 || 'Min 8 characters',
                     incorrectPassword: () => {
+                        if (this.errorType === 'password') return this.errorMessage;
+                        else return false
+                    },
+                    match: v => v === this.password || "Passwords don't match",
+                    nonValidPassword: () => {
                         if (this.errorType === 'password') return this.errorMessage;
                         else return false
                     },
@@ -76,13 +124,13 @@
         watch: {
             email() {
                 this.$refs.form.resetValidation();
-                this.errorType = undefined;
-                this.errorMessage = undefined;
+                this.errorType = null;
+                this.errorMessage = null;
             },
             password() {
                 if (this.errorType === 'password') {
-                    this.errorType = undefined;
-                    this.errorMessage = undefined
+                    this.errorType = null;
+                    this.errorMessage = null
                 }
             }
         },
@@ -94,16 +142,25 @@
         methods: {
             validate() {
                 if (this.$refs.form.validate()) {
-                    auth.loginWithEmail(this).then(() => {
-                    }).catch(error => {
-                        this.errorMessage = error.message;
-                        if (this.errorMessage === 'Email and password don\'t match') {
-                            this.errorType = 'password'
-                        } else {
-                            this.errorType = 'email'
-                        }
-                        this.$refs.form.validate()
-                    })
+                    if (this.signUp) {
+                        auth.signUpWithEmail(this).then(() => {
+                        }).catch(error => {
+                            this.errorMessage = error.message;
+                            this.errorType = 'email';
+                            this.$refs.form.validate()
+                        })
+                    } else {
+                        auth.loginWithEmail(this).then(() => {
+                        }).catch(error => {
+                            this.errorMessage = error.message;
+                            if (this.errorMessage === 'Email and password don\'t match') {
+                                this.errorType = 'password'
+                            } else {
+                                this.errorType = 'email'
+                            }
+                            this.$refs.form.validate()
+                        })
+                    }
                 }
             },
             checkToken() {
@@ -114,6 +171,16 @@
                     notifications.addNotification(error.response.data.error);
                     this.$router.replace('/')
                 })
+            },
+            changeView() {
+                this.password = '';
+                this.confirmPassword = '';
+                this.errorMessage = null;
+                this.errorType = null;
+                this.showPassword = false;
+                this.showConfirmPassword = false;
+                this.signUp = !this.signUp;
+                this.$refs.form.resetValidation();
             }
         }
     }
@@ -123,5 +190,10 @@
     .login {
         border-radius: 4px;
         text-transform: none !important;
+    }
+
+    .button {
+        text-transform: None !important;
+        text-decoration: underline;
     }
 </style>
