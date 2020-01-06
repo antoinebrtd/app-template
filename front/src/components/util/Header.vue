46d81c0d13410c20c843b5706d44f875c543c963
antoinebrtd <abertrand830@gmail.com>
2020-01-06 13:24:06 +0100

    Make nice profile menu

--- front/src/components/util/Header.vue
+++ front/src/components/util/Header.vue
@@ -1,120 +1,77 @@
 <template>
   <div class="header">
-    <transition name="fade" appear>
-      <v-toolbar fixed :class="transparentHeader ? 'menu-header-transparent elevation-0' : 'menu-header elevation-5'">
-        <v-toolbar-items>
-          <router-link to="/home" class="px-3 py-2">
-            <v-img src="../../assets/logos/vue.png" alt="logo" class="logo"></v-img>
-          </router-link>
-        </v-toolbar-items>
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="pr-4" v-if="$data.$_profile">
-          <v-scroll-x-reverse-transition v-for="link in links" :key="link.text">
-            <v-btn
-                    v-if="displayMenu"
-                    text
-                    class="px-4 main-menu pt-1"
-                    :to="link.route"
-                    :class="transparentHeader ? 'white--text' : ''"
-            >
-              {{ link.text }}
-            </v-btn>
-          </v-scroll-x-reverse-transition>
-          <v-scroll-x-reverse-transition>
-            <v-btn
-                    v-if="displayMenu"
-                    class="px-4 main-menu pt-1"
-                    :class="transparentHeader ? 'primary--text' : 'tertiary--text'"
-                    to="/Contact"
-                    text>
-              Contact
-            </v-btn>
-          </v-scroll-x-reverse-transition>
-          <v-slide-x-transition>
-            <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="300" offset-x>
-              <template v-slot:activator="{ on }">
-                <v-btn v-on="on" fab icon small class="ml-5">
-                  <v-avatar :size="36">
-                    <img v-if="$data.$_profile.picture" :src="$data.$_profile.picture" :alt="$data.$_profile.name">
-                    <v-icon v-else large :color="transparentHeader ? 'white' : ''">account_circle</v-icon>
-                  </v-avatar>
-                </v-btn>
-              </template>
+    <v-toolbar fixed :class="transparentHeader ? 'menu-header-transparent elevation-0' : 'menu-header elevation-5'">
 
-              <v-card>
-                <v-list>
-                  <v-list-item avatar>
-                    <v-list-item-avatar>
-                      <img v-if="$data.$_profile.picture" :src="$data.$_profile.picture" :alt="$data.$_profile.name">
-                      <v-icon v-else large>account_circle</v-icon>
-                    </v-list-item-avatar>
+      <v-toolbar-items>
+        <router-link to="/home" class="px-3 py-2">
+          <v-img src="../../assets/logos/vue.png" alt="logo" class="logo"></v-img>
+        </router-link>
+      </v-toolbar-items>
 
-                    <v-list-item-group>
-                      <v-list-item-group>{{ $data.$_profile.name }}</v-list-item-group>
-                    </v-list-item-group>
-                    <v-spacer></v-spacer>
-                    <v-list-item-icon>
-                      <v-icon v-if="user.accountActivated" class="mt-1" small color="primary">verified_user</v-icon>
-                    </v-list-item-icon>
-                  </v-list-item>
-                </v-list>
+      <v-spacer></v-spacer>
 
-                <v-divider></v-divider>
+      <v-toolbar-items v-if="$data.$_profile">
+        <v-scroll-x-reverse-transition v-for="link in links" :key="link.text">
+          <v-btn
+                  v-if="displayMenu"
+                  text
+                  class="px-4 main-menu pt-1"
+                  :to="link.route"
+                  :class="transparentHeader ? 'white--text' : ''"
+          >
+            {{ link.text }}
+          </v-btn>
+        </v-scroll-x-reverse-transition>
+        <v-scroll-x-reverse-transition>
+          <v-btn
+                  v-if="displayMenu"
+                  class="px-4 main-menu pt-1"
+                  :class="transparentHeader ? 'primary--text' : 'tertiary--text'"
+                  to="/Contact"
+                  text>
+            Contact
+          </v-btn>
+        </v-scroll-x-reverse-transition>
+      </v-toolbar-items>
 
-                <v-list>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-switch v-model="jobs" color="primary" class="mt-2 ml-3"></v-switch>
-                    </v-list-item-action>
-                    <v-list-item-group class="text-xs-right">Enable jobs</v-list-item-group>
-                  </v-list-item>
-                </v-list>
-
-                <v-card-actions class="pa-3">
-                  <v-spacer></v-spacer>
-                  <v-btn color="error" text @click="logout">Logout</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-menu>
-          </v-slide-x-transition>
-        </v-toolbar-items>
-        <div v-else style="height: 100%;" class="pa-3">
-          <v-progress-circular indeterminate></v-progress-circular>
-        </div>
-      </v-toolbar>
-    </transition>
+      <v-toolbar-items class="pr-4 ml-2">
+        <profile-menu :transparentHeader="transparentHeader"></profile-menu>
+      </v-toolbar-items>
+    </v-toolbar>
 
     <jobs v-if="jobs"></jobs>
+
     <v-snackbar v-model="activationReminder" :timeout="0" color="primary">
       Seems like you haven't activated your account yet!
       <v-btn text @click="resendEmail" style="text-transform: none; text-decoration: underline">Resend email</v-btn>
-       <v-tooltip right>
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" text icon @click="activationReminder = false" v-on="on">
-              <v-icon color="white">clear</v-icon>
-            </v-btn>
-          </template>
-          <span>Close</span>
-        </v-tooltip>
+      <v-tooltip right>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" text icon @click="activationReminder = false" v-on="on">
+            <v-icon color="white">clear</v-icon>
+          </v-btn>
+        </template>
+        <span>Close</span>
+      </v-tooltip>
     </v-snackbar>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
-  import auth from "@/modules/auth";
   import notifications from "@/modules/notifications";
+  import auth from "@/modules/auth";
+
 
   import Jobs from './Jobs';
+  import ProfileMenu from "./ProfileMenu";
 
   export default {
     name: 'Header',
-    components: {Jobs},
+    components: {ProfileMenu, Jobs},
     data() {
       return {
-        jobs: false,
-        menu: false,
         user: auth.user,
+        jobs: true,
         activationReminder: false,
         scrolled: false,
         displayMenu: false,
@@ -143,12 +100,9 @@
       window.removeEventListener('scroll', this.handleScroll);
     },
     methods: {
-      logout() {
-        auth.logout();
-      },
       resendEmail() {
         this.activationReminder = false;
-        axios.post(process.env.VUE_APP_EMAIL_AUTH_URL + '/resend-email').then( response => {
+        axios.post(process.env.VUE_APP_EMAIL_AUTH_URL + '/resend-email').then(response => {
           notifications.addNotification(response.data)
         }).catch(error => {
           notifications.addNotification(error.response.data.error)
@@ -176,11 +130,6 @@
     background: transparent !important;
     width: 100%
   }
-
-  .subheader-items div {
-    height: 100%;
-  }
-
   .logo {
     height: 50px;
     width: 60px
@@ -195,18 +144,5 @@
 <style>
   .v-toolbar__content {
     padding: 0 !important;
-  }
-
-  .v-toolbar .v-input__slot {
-    background: transparent !important;
-  }
-
-  .list-complete-enter, .list-complete-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
-  }
-
-  .list-complete-leave-active {
-    position: absolute;
   }
 </style>
---
+++ front/src/components/util/ProfileMenu.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-slide-x-transition>
+        <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="250" offset-x offset-y>
+            <template v-slot:activator="{ on }">
+                <v-btn v-on="on" fab icon small>
+                    <v-avatar :size="36">
+                        <img v-if="$data.$_profile.picture" :src="$data.$_profile.picture" :alt="$data.$_profile.name">
+                        <v-icon v-else large :color="transparentHeader ? 'white' : ''">account_circle</v-icon>
+                    </v-avatar>
+                </v-btn>
+            </template>
+
+            <v-list>
+                <v-list-item @click="$router.push('my-account')" class="py-1">
+                    <v-list-item-avatar size="30" class="ml-2">
+                        <img v-if="$data.$_profile.picture" :src="$data.$_profile.picture"
+                             :alt="$data.$_profile.name">
+                        <v-icon v-else large>account_circle</v-icon>
+                    </v-list-item-avatar>
+
+                    <v-list-item-group>
+                        <v-list-item-group>My account</v-list-item-group>
+                    </v-list-item-group>
+                    <v-spacer></v-spacer>
+                    <v-list-item-icon>
+                        <v-icon v-if="user.accountActivated" small color="tertiary">verified_user</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+                <v-divider color="primary"></v-divider>
+                <v-list-item @click="$router.push('settings')">
+                    <v-list-item-icon>
+                        <v-icon small color="primary" class="ml-4">settings</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-group>
+                        <v-list-item-group>Settings</v-list-item-group>
+                    </v-list-item-group>
+                </v-list-item>
+                <v-list-item @click="logout">
+                    <v-list-item-icon>
+                        <v-icon small color="error" class="ml-4">logout</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-group>
+                        <v-list-item-group>Logout</v-list-item-group>
+                    </v-list-item-group>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-slide-x-transition>
+</template>
+
+<script>
+    import auth from "@/modules/auth";
+
+    export default {
+        name: "ProfileMenu",
+        props: {
+            transparentHeader: Boolean
+        },
+        data() {
+            return {
+                user: auth.user,
+                menu: false
+            }
+        },
+        methods: {
+            logout() {
+                auth.logout();
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
